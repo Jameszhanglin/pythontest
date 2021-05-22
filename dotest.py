5914260456b617808f96bf08c042bc5dcb9f8c70
Jameszhanglin <76399835+Jameszhanglin@users.noreply.github.com>
2021-05-22 17:13:23 +0800

    上传提交
    Signed-off-by: Jameszhanglin <76399835+Jameszhanglin@users.noreply.github.com>

--- dotest.py
+++ dotest.py
@@ -1,4 +1,4 @@
-def count(list):
+def count(list):  # 输出数组中的个数
     if list == []:
         return 0
     else:
@@ -7,7 +7,7 @@ def count(list):
 
 print(count([2, 3, 5, 6, 1, 10]))
 
-
+#找数组中的最大值
 def findMax(list):
     if len(list) == 2:
         return list[0] if list[0] > list[1] else list[1]
@@ -16,3 +16,17 @@ def findMax(list):
 
 
 print(findMax([1, 2, 4, 5, 8, 10]))
+
+
+# 快速排序
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        grater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(grater)
+
+
+print(quicksort([10, 5, 2, 3]))
